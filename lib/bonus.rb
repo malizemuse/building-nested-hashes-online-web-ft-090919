8efe6f8c8c880b2f59f46d80cedd3d7c7bd85e13
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 23:51:23 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,11 +23,11 @@ def bonus
   #code your solution here:
   
   # Changes Romeo and Juliet's statuses to dead
-  puts epic_tragedy[:montague][:hero][:status] = "dead"
-  puts epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  #epic_tragedy
+  epic_tragedy
 end
 
 # TEST CODE
